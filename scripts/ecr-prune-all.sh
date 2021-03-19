8a7d6b4fc566f868b5cd72eec03f05963b22e7e9
Ray C Horn <raychorn@gmail.com>
2021-03-19 13:21:27 -0600

    updates.

--- scripts/ecr-prune-all.sh
+++ scripts/ecr-prune-all.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 venv=$(ls ./.venv*/bin/activate)
-. $venv
+
+if [[ -f $venv ]]
+then
+    . $venv
+else
+    echo "Cannot find $venv, please run this command in the correct directory."
+    exit
+fi
 
 python ./auto_ecr.py --clean-ecr
